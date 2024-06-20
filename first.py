94f04afddc319554a5a1cb8e491497585d316bc0
Natan Asrat <nathanyilmaasrat@gmail.com>
2024-06-20 11:35:36 +0000

    use dotenv, change prompt, add streamlit logic if cuisine

--- first.py
+++ first.py
@@ -2,10 +2,16 @@ import streamlit as st
 import langchain_helper
 st.title("Restaurant Name Generator")
 
-cuisine = st.sidebar.checkbox("Pick a cuisine", (
+cuisine = st.sidebar.selectbox("Pick a cuisine", (
     "Italian",
     "Ethiopian",
     "Mexican",
     "American"
 ))
-
+if cuisine:
+    response = langchain_helper.generate_restaurant_name_and_items(cuisine)
+    st.header(response['restaurant_name'].strip())
+    st.write("**Menu Items**")
+    menu_items = response['menu_items'].strip().split(",")
+    for item in menu_items:
+        st.write("-", item)
--- langchain_helper.py
+++ langchain_helper.py
@@ -2,19 +2,21 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains import LLMChain
 import os
+from dotenv import load_dotenv
+load_dotenv()
 from langchain.chains import SequentialChain
 
-os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
+# os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
 
 prompt1 = ChatPromptTemplate.from_messages(
     [
-        ("system", "Return as a comma separated string"),
-        ("user", "I want to open a restauran for {cuisine}")
+        ("system", "Only answer with no description and with no disclaimers or notes."),
+        ("user", "I want to open a restaurant for {cuisine}. Give me a restaurant name.")
     ]
 )
 prompt2 = ChatPromptTemplate.from_messages(
     [
-        ("system", "Return as a comma separated string"),
+        ("system", "Return as a comma separated string. Dont return other details."),
         ("user", "Suggest some menu items for {restaurant_name}")
     ]
 )
